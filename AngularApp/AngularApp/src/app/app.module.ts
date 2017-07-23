6e3a5372d24c33c041ef6c3823a22ca407decc39
DESKTOP-JB56716\Egor <Egorkam.lp@gmail.com>
2017-07-24 02:14:17 +0300

    initialCommit

---
+++ AngularApp/AngularApp/src/app/app.module.ts
@@ -0,0 +1,20 @@
+﻿import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { ServerComponent} from './server/server.component'
+@NgModule({
+   declarations: [
+       AppComponent,
+       ServerComponent
+   ],
+   imports: [
+      BrowserModule,
+      FormsModule,
+      HttpModule
+   ],
+   providers: [],
+   bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ AngularApp/AngularApp/src/app/server/server.component.ts
@@ -0,0 +1,16 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http'
+@Component({
+    selector: 'app-server',
+    templateUrl: './server/server.component.html',
+    styleUrls: ['./server/server.component.css']
+})
+export class ServerComponent implements OnInit {
+    constructor(private _httpService: Http) { }
+    apiValues: string[] = [];
+    ngOnInit() {
+        this._httpService.get('/api/else').subscribe(values => {
+            this.apiValues = values.json() as string[];
+        });
+    }
+}
